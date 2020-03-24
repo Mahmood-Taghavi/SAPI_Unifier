6c0e3e16ddfc7ae9822007b4e873ca955d71ccb4
Mahmood Taghavi <mahmood.taghavi@gmail.com>
2020-03-24 06:54:07 +0430

    version 1.0
    publishing of version 1.0 which works fine.

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SAPI_Unifier
+{
+	partial class Form_main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox_report = new System.Windows.Forms.TextBox();
+			this.label_info = new System.Windows.Forms.Label();
+			this.button_exit = new System.Windows.Forms.Button();
+			this.button_about = new System.Windows.Forms.Button();
+			this.linkLabel_website = new System.Windows.Forms.LinkLabel();
+			this.SuspendLayout();
+			// 
+			// textBox_report
+			// 
+			this.textBox_report.Location = new System.Drawing.Point(18, 38);
+			this.textBox_report.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textBox_report.Multiline = true;
+			this.textBox_report.Name = "textBox_report";
+			this.textBox_report.ReadOnly = true;
+			this.textBox_report.Size = new System.Drawing.Size(822, 187);
+			this.textBox_report.TabIndex = 1;
+			// 
+			// label_info
+			// 
+			this.label_info.AutoSize = true;
+			this.label_info.Location = new System.Drawing.Point(18, 14);
+			this.label_info.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label_info.Name = "label_info";
+			this.label_info.Size = new System.Drawing.Size(814, 20);
+			this.label_info.TabIndex = 0;
+			this.label_info.Text = "This program unifies installed Microsoft OneCore voices and Microsoft Speech Serv" +
+    "er voices with Microsoft SAPI 5.";
+			// 
+			// button_exit
+			// 
+			this.button_exit.Location = new System.Drawing.Point(628, 235);
+			this.button_exit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.button_exit.Name = "button_exit";
+			this.button_exit.Size = new System.Drawing.Size(112, 35);
+			this.button_exit.TabIndex = 4;
+			this.button_exit.Text = "Exit";
+			this.button_exit.UseVisualStyleBackColor = true;
+			this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
+			// 
+			// button_about
+			// 
+			this.button_about.Location = new System.Drawing.Point(102, 235);
+			this.button_about.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.button_about.Name = "button_about";
+			this.button_about.Size = new System.Drawing.Size(156, 35);
+			this.button_about.TabIndex = 2;
+			this.button_about.Text = "About SAPI Unifier";
+			this.button_about.UseVisualStyleBackColor = true;
+			this.button_about.Click += new System.EventHandler(this.button_about_Click);
+			// 
+			// linkLabel_website
+			// 
+			this.linkLabel_website.AutoSize = true;
+			this.linkLabel_website.Location = new System.Drawing.Point(361, 242);
+			this.linkLabel_website.Name = "linkLabel_website";
+			this.linkLabel_website.Size = new System.Drawing.Size(149, 20);
+			this.linkLabel_website.TabIndex = 3;
+			this.linkLabel_website.TabStop = true;
+			this.linkLabel_website.Text = "Visit project website";
+			this.linkLabel_website.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_website_LinkClicked);
+			// 
+			// Form_main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(852, 285);
+			this.Controls.Add(this.linkLabel_website);
+			this.Controls.Add(this.button_about);
+			this.Controls.Add(this.button_exit);
+			this.Controls.Add(this.label_info);
+			this.Controls.Add(this.textBox_report);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.Name = "Form_main";
+			this.Text = "SAPI Unifier";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox_report;
+		private System.Windows.Forms.Label label_info;
+		private System.Windows.Forms.Button button_exit;
+		private System.Windows.Forms.Button button_about;
+		private System.Windows.Forms.LinkLabel linkLabel_website;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32; // it's required for reading and writing to the windows registry
+
+namespace SAPI_Unifier
+{
+	public partial class Form_main : Form
+	{
+		public Form_main()
+		{
+			InitializeComponent();
+
+			string SAPI5Path = "SOFTWARE\\Microsoft\\SPEECH\\Voices\\Tokens\\";
+			string MobilePath = "SOFTWARE\\Microsoft\\Speech_OneCore\\Voices\\Tokens\\";
+			string ServerPath = "SOFTWARE\\Microsoft\\Speech Server\\v11.0\\Voices\\Tokens\\";
+			string CortanaPath = "SOFTWARE\\Microsoft\\Speech_OneCore\\CortanaVoices\\Tokens\\";
+
+			if (System.Environment.Is64BitOperatingSystem)
+			{
+				SAPI5Path = "SOFTWARE\\WOW6432Node\\Microsoft\\SPEECH\\Voices\\Tokens\\";
+				MobilePath = "SOFTWARE\\WOW6432Node\\Microsoft\\Speech_OneCore\\Voices\\Tokens\\";
+				ServerPath = "SOFTWARE\\WOW6432Node\\Microsoft\\Speech Server\\v11.0\\Voices\\Tokens\\";
+			}
+
+	        string[] SAPI5Tokens = new string[] { };
+		    string[] MobileTokens = new string[] { };
+			string[] ServerTokens = new string[] { };
+			string[] CortanaTokens = new string[] { };
+			
+			try
+			{
+				//// Fix possible inconsistencies in SAPI 5 voices due to some uninstalled mobile or server voices
+				RegistryKey key = Registry.LocalMachine.OpenSubKey(SAPI5Path);
+		        if (key != null) SAPI5Tokens = key.GetSubKeyNames();
+				key = Registry.LocalMachine.OpenSubKey(MobilePath);
+				if (key != null) MobileTokens = key.GetSubKeyNames();
+				key = Registry.LocalMachine.OpenSubKey(ServerPath);
+				if (key != null) ServerTokens = key.GetSubKeyNames();
+				key = Registry.LocalMachine.OpenSubKey(CortanaPath);
+				if (key != null) CortanaTokens = key.GetSubKeyNames();
+			}
+			catch
+			{
+				MessageBox.Show("An Error in the preliminary analysis is occurred!", "Error #1");
+			}
+
+			foreach (string SAPI5Token in SAPI5Tokens)
+			{
+				//MessageBox.Show(SAPI5Token);
+				//MessageBox.Show(Array.IndexOf(MobileTokens, SAPI5Token).ToString());
+				try
+				{
+					// Remove uninstalled oneCore voices from the SAPI 5 interface
+					if (SAPI5Token.Contains("MSTTS_V110_"))
+					{
+						if (Array.IndexOf(MobileTokens, SAPI5Token) < 0 & Array.IndexOf(CortanaTokens, SAPI5Token) < 0)
+						{
+							//MessageBox.Show(SAPI5Token);
+							RegistryKey key = Registry.LocalMachine.OpenSubKey(SAPI5Path + "\\" + SAPI5Token);
+							string VoiceName = key.GetValue("").ToString();
+							textBox_report.Text = VoiceName + " which was an uninstalled OneCore voice is removed from SAPI 5." + Environment.NewLine;
+							key = Registry.LocalMachine.OpenSubKey(SAPI5Path, true); // request write permission
+							key.DeleteSubKeyTree(SAPI5Token, false); // do not raise an error if the subkey be missing
+						}
+					}
+				}
+				catch
+				{
+					MessageBox.Show("An Error in removing of " + SAPI5Token + " is occurred!", "Error #2");
+				}
+
+				try
+				{
+					// Remove uninstalled SpeechServer voices from the SAPI 5 interface
+					if (SAPI5Token.Contains("TTS_MS_"))
+					{
+						if (Array.IndexOf(ServerTokens, SAPI5Token) < 0)
+						{
+							//MessageBox.Show(SAPI5Token);
+							RegistryKey key = Registry.LocalMachine.OpenSubKey(SAPI5Path + "\\" + SAPI5Token);
+							string VoiceName = key.GetValue("").ToString();
+							if (VoiceName.Contains("Microsoft Server Speech Text to Speech Voice"))
+							{
+								textBox_report.Text = VoiceName + " which was an uninstalled Speech Server voice is removed from SAPI 5." + Environment.NewLine;
+								key = Registry.LocalMachine.OpenSubKey(SAPI5Path, true);
+								key.DeleteSubKeyTree(SAPI5Token, false);
+							}
+						}
+					}
+				}
+				catch
+				{
+					MessageBox.Show("An Error in removing of " + SAPI5Token + " is occurred!", "Error #3");
+				}
+			}
+
+			//// Now that all things become OK it's time to add OneCore voices to SAPI 5 interface
+			foreach (string MobileToken in MobileTokens)
+			{
+				try
+				{
+					if (Array.IndexOf(SAPI5Tokens, MobileToken) < 0)
+					{
+						//MessageBox.Show(MobileToken);
+						// Create a SAPI5Token with the same name of the MobileToken
+						RegistryKey dstkey = Registry.LocalMachine.OpenSubKey(SAPI5Path, true);
+						dstkey.CreateSubKey(MobileToken);
+						// Copy values from the MobileToken to the created SAPI5Token
+						RegistryKey srckey = Registry.LocalMachine.OpenSubKey(MobilePath + "\\" + MobileToken);
+						dstkey = Registry.LocalMachine.OpenSubKey(SAPI5Path + "\\" + MobileToken, true);
+						foreach (string name in srckey.GetValueNames())
+						{
+							//MessageBox.Show(name);
+							if (name == "")
+							{
+								textBox_report.Text = textBox_report.Text + srckey.GetValue(name) + " is added from OneCore Speech API to SAPI 5." + Environment.NewLine;
+							}
+							dstkey.SetValue(name, srckey.GetValue(name), srckey.GetValueKind(name));
+						}
+						// Create the Attributes subkey for the created voice Token
+						dstkey = Registry.LocalMachine.OpenSubKey(SAPI5Path + "\\" + MobileToken, true);
+						dstkey.CreateSubKey("Attributes");
+						// Copy values from the Attributes subkey of the MobileToken to the created Attributes subkey of SAPI5Token
+						srckey = Registry.LocalMachine.OpenSubKey(MobilePath + "\\" + MobileToken + "\\" + "Attributes");
+						dstkey = Registry.LocalMachine.OpenSubKey(SAPI5Path + "\\" + MobileToken + "\\" + "Attributes", true);
+						foreach (string name in srckey.GetValueNames())
+						{
+							dstkey.SetValue(name, srckey.GetValue(name), srckey.GetValueKind(name));
+						}
+					}
+				}
+				catch
+				{
+					MessageBox.Show("An Error in adding of " + MobileToken + " is occurred!", "Error #4");
+				}
+			}
+			
+			//// Now that all things become OK it's time to add SpeechServer voices to SAPI 5 interface
+			foreach (string ServerToken in ServerTokens)
+			{
+				try
+				{
+					if (Array.IndexOf(SAPI5Tokens, ServerToken) < 0)
+					{
+						//MessageBox.Show(MobileToken);
+						// Create a SAPI5Token with the same name of the MobileToken
+						RegistryKey dstkey = Registry.LocalMachine.OpenSubKey(SAPI5Path, true);
+						dstkey.CreateSubKey(ServerToken);
+						// Copy values from the MobileToken to the created SAPI5Token
+						RegistryKey srckey = Registry.LocalMachine.OpenSubKey(ServerPath + "\\" + ServerToken);
+						dstkey = Registry.LocalMachine.OpenSubKey(SAPI5Path + "\\" + ServerToken, true);
+						foreach (string name in srckey.GetValueNames())
+						{
+							//MessageBox.Show(name);
+							if (name == "")
+							{
+								textBox_report.Text = textBox_report.Text + srckey.GetValue(name) + " is added from Speech Server Speech API to SAPI 5." + Environment.NewLine;
+							}
+							dstkey.SetValue(name, srckey.GetValue(name), srckey.GetValueKind(name));
+						}
+						// Create the Attributes subkey for the created voice Token
+						dstkey = Registry.LocalMachine.OpenSubKey(SAPI5Path + "\\" + ServerToken, true);
+						dstkey.CreateSubKey("Attributes");
+						// Copy values from the Attributes subkey of the MobileToken to the created Attributes subkey of SAPI5Token
+						srckey = Registry.LocalMachine.OpenSubKey(ServerPath + "\\" + ServerToken + "\\" + "Attributes");
+						dstkey = Registry.LocalMachine.OpenSubKey(SAPI5Path + "\\" + ServerToken + "\\" + "Attributes", true);
+						foreach (string name in srckey.GetValueNames())
+						{
+							dstkey.SetValue(name, srckey.GetValue(name), srckey.GetValueKind(name));
+						}
+					}
+				}
+				catch
+				{
+					MessageBox.Show("An Error in adding of " + ServerToken + " is occurred!", "Error #5");
+				}
+
+			}
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			if (textBox_report.Text == "")
+			{
+				textBox_report.Text = "It seems everything is OK. Please don't forget to re-run the SAPI Unifier after installing or uninstalling of any OneCore or Speech Server voices.";
+			}
+		}
+
+		private void button_exit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void button_about_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Author: Mahmood Taghavi; Version 1.0; License: GPL 3", "About SAPI Unifier");
+		}
+
+		private void linkLabel_website_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			// Change the color of the link text by setting LinkVisited
+			// to true.
+			linkLabel_website.LinkVisited = true;
+			//Call the Process.Start method to open the default browser
+			//with a URL:
+			System.Diagnostics.Process.Start("https://mahmood-taghavi.github.io/SAPI_Unifier/");
+		}
+	}
+}
